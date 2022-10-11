4ee951958c768acaae5b356a5895e9733ca34d62
vdevcode <thanhvumaidao1234567@gmail.com>
2022-10-11 12:44:59 +0700

    source

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ buttonOpenMenu.addEventListener('click',()=>{
   showIconMenu.classList.toggle('active')
   
 })
+
+let hoverIconLove = document.querySelector('.card-action-btn')
+let showIconLove = document.querySelector('.card-action-btn-love')
+
+hoverIconLove.addEventListener('click',()=>{
+  showIconLove.style.color = 'red'
+})
+
+var let = new Swiper(".tour-places .swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+     0: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+     },
+     576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+     },
+     992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+     },
+     1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+     },
+  },
+});
